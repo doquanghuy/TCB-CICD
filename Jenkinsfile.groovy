16b53bb55d0cb1fb7b7d026ee26a1ece594d7eeb
huydq10 <huydq10@techcombank.com.vn>
2020-10-14 16:55:46 +0700

    add jenkinsfile

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,29 @@
+pipeline {
+    agent any
+    stage('Checkout/Build/Test') {
+
+        // Checkout files.
+        checkout([
+            $class: 'GitSCM',
+            branches: [[name: 'main']],
+            doGenerateSubmoduleConfigurations: false,
+            extensions: [], submoduleCfg: [],
+            userRemoteConfigs: [[
+                name: 'github',
+                url: 'git@github.com:doquanghuy/TCB-CICD.git'
+            ]]
+        ])
+
+        // Build and Test
+        sh 'xcodebuild -scheme "TimeTable" -configuration "Debug" build test -destination "platform=iOS Simulator,name=iPhone 6,OS=10.1" -enableCodeCoverage YES | /usr/local/bin/xcpretty -r junit'
+
+        // Publish test restults.
+        step([$class: 'JUnitResultArchiver', allowEmptyResults: true, testResults: 'build/reports/junit.xml'])
+    }
+
+    stage('Analytics') {
+        sh '/usr/local/bin/slather coverage --jenkins --html --scheme TCB-CICD TCB-CICD.xcodeproj/'
+        // Publish coverage results
+        publishHTML([allowMissing: false, alwaysLinkToLastBuild: false, keepAll: false, reportDir: 'html', reportFiles: 'index.html', reportName: 'Coverage Report'])
+    }
+}
